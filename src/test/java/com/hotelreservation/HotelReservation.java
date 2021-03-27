05ca393f275fdcbd20e0e4670af354d255b11077
unknown <suyashgaikwad31@gmail.com>
2021-03-27 09:15:52 +0530

    Welcome to Hotel Reservation System

---
+++ src/test/java/com/hotelreservation/HotelReservation.java
@@ -0,0 +1,7 @@
+package com.hotelreservation;
+
+public class HotelReservation {
+    public static void main(String [] args) {
+        System.out.println("Welcome To Hotel Reservation System");
+    }
+}
